e0d5c967d0b0f142c47853ffbc78942e16d7abaa
Felipe Soares Coelho <felipecoelho.ness@gmail.com>
2023-10-06 10:38:54 -0300

    Finalizando a pagina inicial

--- capputeeno/src/components/products-list.tsx
+++ capputeeno/src/components/products-list.tsx
@@ -1,17 +1,31 @@
 "use client";
 
 import { useProducts } from "@/hooks/useProducts";
+import { ProductCard } from "./products-card";
+import styled from "styled-components";
 
-interface ProductsListProps {
+const ListContainer = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, 256px);
+  grid-gap: 32px;
+  max-width: 100%;
 
-};
+  margin-top: 32px;
+`
 
-export function ProductsList(props: ProductsListProps) {
+export function ProductsList() {
   const { data } = useProducts();
   console.log(data);
   return (
-    <div>
-      <h1>Products List</h1>
-    </div>
+    <ListContainer>
+      {data?.map(product =>
+        <ProductCard
+          key={product.id}
+          title={product.name}
+          price={product.price_in_cents}
+          image={product.image_url}
+      />
+      )}
+    </ListContainer>
   )
 };
